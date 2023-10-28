34fabc2d0096896634c8f976dfb6a8b41608d6be
siat.testing <siat.testing@gmail.com>
2023-10-28 17:03:23 +0000

    ading lucia auth

--- src/lib/lucia.ts
+++ src/lib/lucia.ts
@@ -1,9 +1,21 @@
 import { lucia } from 'lucia';
 import { astro } from 'lucia/middleware';
+import { libsqlClient } from './db.js';
+import { libsql } from '@lucia-auth/adapter-sqlite';
 
 export const auth = lucia({
   env: import.meta.env.DEV ? 'DEV' : 'PROD',
   middleware: astro(),
+  adapter: libsql(libsqlClient, {
+    user: 'user',
+    session: 'user_session',
+    key: 'user_key',
+  }),
+  getUserAttributes: (data) => {
+    return {
+      username: data.username,
+    };
+  },
 });
 
 export type Auth = typeof auth;
---
+++ src/lib/tasks.js
@@ -0,0 +1,5 @@
+const tasks = [
+  { id: 1, title: 'created basic skeleton' },
+  { id: 2, title: 'added lucia with pnpm add lucia' },
+  { id: 3, title: 'added via pnpm add drizzle-orm postgres' },
+];
