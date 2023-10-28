b428c28e7c6de1925b7d253941c9c433734fbc7f
siat.testing <siat.testing@gmail.com>
2023-10-28 22:06:21 +0000

    added turso and drizzle

--- src/lib/lucia.ts
+++ src/lib/lucia.ts
@@ -1,12 +1,20 @@
 import { lucia } from 'lucia';
+// import { PrismaClient } from '@prisma/client';
 import { astro } from 'lucia/middleware';
-import { libsqlClient } from './db.js';
+import { createClient } from '@libsql/client';
 import { libsql } from '@lucia-auth/adapter-sqlite';
+
+// const client = new PrismaClient();
+
+const tursodb = createClient({
+  url: import.meta.env.TURSO_DB_URL,
+  authToken: import.meta.env.TURSO_DB_AUTH_TOKEN,
+});
 
 export const auth = lucia({
   env: import.meta.env.DEV ? 'DEV' : 'PROD',
   middleware: astro(),
-  adapter: libsql(libsqlClient, {
+  adapter: libsql(tursodb, {
     user: 'user',
     session: 'user_session',
     key: 'user_key',
---
+++ src/lib/prismaclient.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+// 1. Import libSQL and the Prisma libSQL driver adapter
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
+import { createClient } from '@libsql/client';
+
+// 2. Instantiate libSQL
+const libsql = createClient({
+  // @ts-expect-error
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
+// 3. Instantiate the libSQL driver adapter
+const adapter = new PrismaLibSQL(libsql);
+// Pass the adapter option to the Prisma Client instance
+const prisma = new PrismaClient({ adapter });
